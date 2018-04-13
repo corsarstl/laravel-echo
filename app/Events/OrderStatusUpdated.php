7faeeb91aa7cd698e94c4a14d3a13958cc137aa0
Syromiatnykov Vitalii <corsarstl@gmail.com>
2018-04-13 14:17:23 +0300

    Lesson 2. 'Pushing to Pusher'

--- app/Events/OrderStatusUpdated.php
+++ app/Events/OrderStatusUpdated.php
@@ -3,25 +3,25 @@
 namespace App\Events;
 
 use Illuminate\Broadcasting\Channel;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
 
 class OrderStatusUpdated implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
+    public $user;
+
     /**
      * Create a new event instance.
      *
-     * @return void
+     * @param $user
      */
-    public function __construct()
+    public function __construct($user)
     {
-        //
+        $this->user = $user;
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 use App\Events\OrderStatusUpdated;
 
 Route::get('/', function () {
-    OrderStatusUpdated::dispatch();
+    $user = factory(App\User::class)->create();
+
+    OrderStatusUpdated::dispatch($user);
 /*    the same as
     event(new OrderStatusUpdated());*/
     return view('welcome');
